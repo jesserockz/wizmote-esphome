dfc79c9a1c8b276252676f8cf1f9d4869222cc29
Zsolt Kovacs <kzsolt@kzsolt.info>
2023-03-05 10:41:18 +0100

    Test

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -4,16 +4,28 @@ namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence <= this->last_sequence_ && wizmote.bssid == this->last_bssid_)
+
+  bool isequal = true;
+  
+  for(int i=0; i < 6; i++) {
+
+    if(this->last_bssid_[i] != wizmote.bssid[i]){
+      isequal = false;
+      break;
+    }
+
+  }
+
+  if (isequal && wizmote.sequence <= this->last_sequence_)
     return;
   
-  this->last_sequence_ = wizmote.sequence;
+  this->last_sequence_ = this->last_bssid_[i];
 
   for(i=0;i<6;i++)
   {
     this->last_bssid_[i]=wizmote.bssid[i];
   }
-  
+
   this->on_button_->trigger(wizmote);
 }
 }  // namespace wizmote
