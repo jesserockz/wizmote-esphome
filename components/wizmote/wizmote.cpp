58ff93a86e577435328c05bf048395c4c6f9c38f
kzsolt87 <kzsolt@kzsolt.info>
2023-03-05 09:50:40 +0100

    Update wizmote.cpp

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -4,9 +4,6 @@ namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence <= this->last_sequence_)
-    return;
-
   this->last_sequence_ = wizmote.sequence;
   this->on_button_->trigger(wizmote);
 }
