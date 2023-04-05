c6570de6c05791fb7b1b5f9cd372d62bb88c56c6
Jonathan V <11986470+jonofmac@users.noreply.github.com>
2023-04-05 00:06:32 -0500

    Update wizmote.cpp
    Added a 10 element array to store up to 10 remotes worth of bssid's and sequence ids

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -4,11 +4,54 @@ namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence == this->last_sequence_)
+  if (update_wizmote_history(&wizmote))
     return;
 
-  this->last_sequence_ = wizmote.sequence;
   this->on_button_->trigger(wizmote);
 }
+
+boolean update_wizmote_history(WizMotePacket *packet)
+{
+  uint8_t index = 0;
+  if (find_bssid_index(packet, &index))
+  {
+    if (this->history[index].sequence == packet->sequence)
+      return true;
+    
+    // We had a matching MAC, but not a matching sequence, 
+    this->history[index].sequence = packet->sequence;
+  }
+  
+  for (uint8_t i = 9; i > 0; i--)
+    memcpy(&(this->history[i]), &(this->history[i-1]), sizeof(WizMoteHistory));
+  this->history[0].sequence = packet->sequence;
+  memcpy(this->history[0].bssid, packet->bssid, 6);
+  
+  return false;
+}
+
+boolean find_bssid_index(WizMotePacket *packet, uint8_t *index)
+{
+  for (uint8_t i = 0; i < 10; i++)
+  {
+    if (is_bssid_equal(packet, &(this->history[i])))
+    {
+      *index = i;
+      return true;
+    }
+  }
+  return false;
+}
+
+boolean is_bssid_equal(WizMotePacket *packet, WizMoteHistory *history)
+{
+  for (uint8_t i = 0; i < 6; i++)
+  {
+    if (packet->bssid[i] != history->bssid[i])
+      return false;
+  }
+  return true;
+}
+  
 }  // namespace wizmote
 }  // namespace esphome
