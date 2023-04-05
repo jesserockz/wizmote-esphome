a73364184b6412797fa5558672ed7b1e56229577
Jonathan V <11986470+jonofmac@users.noreply.github.com>
2023-04-05 00:26:25 -0500

    Update wizmote.cpp

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -22,7 +22,7 @@ bool WizMoteListener::update_wizmote_history(WizMotePacket *packet)
     this->history[index].sequence = packet->sequence;
   }
   
-  for (uint8_t i = 9; i > 0; i--)
+  for (uint8_t i = (WIZMOTEHISTORYSIZE - 1); i > 0; i--)
     memcpy(&(this->history[i]), &(this->history[i-1]), sizeof(WizMoteHistory));
   this->history[0].sequence = packet->sequence;
   memcpy(this->history[0].bssid, packet->bssid, 6);
@@ -32,7 +32,7 @@ bool WizMoteListener::update_wizmote_history(WizMotePacket *packet)
 
 bool WizMoteListener::find_bssid_index(WizMotePacket *packet, uint8_t *index)
 {
-  for (uint8_t i = 0; i < 10; i++)
+  for (uint8_t i = 0; i < WIZMOTEHISTORYSIZE; i++)
   {
     if (is_bssid_equal(packet, &(this->history[i])))
     {
