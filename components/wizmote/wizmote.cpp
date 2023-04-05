40506dc292bca96f4967c5dcfcc1141e27e0d8fc
Jonathan V <11986470+jonofmac@users.noreply.github.com>
2023-04-04 21:49:35 -0500

    Updated wizmote.cpp to support multiple remotes

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -4,7 +4,7 @@ namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence <= this->last_sequence_)
+  if (wizmote.sequence == this->last_sequence_)
     return;
 
   this->last_sequence_ = wizmote.sequence;
