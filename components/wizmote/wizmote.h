e2c85a6fb06e2bdf2dfb4c218aaf4c049d794de5
Colin Kuebler <kueblc@gmail.com>
2025-06-23 07:24:27 -0400

    Simplify handling of multiple WizMotes (#16)
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- components/wizmote/wizmote.h
+++ components/wizmote/wizmote.h
@@ -4,15 +4,18 @@
 #include "esphome/core/automation.h"
 #include "esphome/core/helpers.h"
 
-#include <vector>
+#include <map>
 
 namespace esphome {
 namespace wizmote {
 
-static const uint8_t WIZMOTEHISTORYSIZE = 20;
+typedef uint64_t MACAddress;
 
 typedef struct __attribute__((__packed__)) WizMotePacket {
-  uint8_t bssid[6];
+  union {
+    uint8_t bssid[6];
+    MACAddress mac_address;
+  };
 
   union {
     uint8_t program;    // 0x91 for ON button, 0x81 for all others
@@ -36,11 +39,6 @@ typedef struct __attribute__((__packed__)) WizMotePacket {
     return packet;
   }
 } WizMotePacket;
-  
-typedef struct WizMoteHistory {
-  uint8_t bssid[6];
-  uint32_t sequence;
-} WizMoteHistory;
 
 class WizMoteListener : public esp_now::ESPNowListener {
  public:
@@ -49,13 +47,9 @@ class WizMoteListener : public esp_now::ESPNowListener {
   Trigger<WizMotePacket> *get_on_button_trigger() { return this->on_button_; }
 
  protected:
-  bool update_wizmote_history(WizMotePacket *packet);
-  bool find_bssid_index(WizMotePacket *packet, uint8_t *index);
-  bool is_bssid_equal(WizMotePacket *packet, WizMoteHistory *history);
-  WizMoteHistory history[WIZMOTEHISTORYSIZE] = {0,};
+  std::map<MACAddress, uint32_t> last_sequence_;
   Trigger<WizMotePacket> *on_button_ = new Trigger<WizMotePacket>();
 };
-  
 
 }  // namespace wizmote
 }  // namespace esphome
