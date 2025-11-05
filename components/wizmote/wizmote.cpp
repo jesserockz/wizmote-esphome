ca582d6857cc118cdccace38ddf22cd14338c122
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-11-06 09:12:14 +1300

    Update to use ESPHome built in espnow component

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -1,17 +1,38 @@
 #include "wizmote.h"
 
-namespace esphome {
-namespace wizmote {
-void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
-  WizMotePacket wizmote = WizMotePacket::build(packet);
+#include "esphome/core/log.h"
+
+namespace esphome::wizmote {
+
+static const char *const TAG = "wizmote";
+
+WizMotePacket WizMotePacket::build(const espnow::ESPNowRecvInfo &info, const uint8_t *data, uint8_t size) {
+  WizMotePacket packet;
+  memcpy(packet.bssid, info.src_addr, ESP_NOW_ETH_ALEN);
+  memcpy(&packet.program, data, size);
+  return packet;
+}
+
+void WizMoteListener::setup() { espnow::global_esp_now->register_broadcasted_handler(this); }
+
+bool WizMoteListener::on_broadcasted(const espnow::ESPNowRecvInfo &info, const uint8_t *data, uint8_t size) {
+  WizMotePacket wizmote = WizMotePacket::build(info, data, size);
+
+  ESP_LOGV(TAG, "Received from %s - %s", format_mac_address_pretty(wizmote.bssid).c_str(),
+           format_hex_pretty(data, size).c_str());
+
+  if (wizmote.program != 0x91 && wizmote.program != 0x81) {
+    ESP_LOGD(TAG, "Ignoring packet with program %02X", wizmote.program);
+    return false;
+  }
 
   if (this->last_sequence_[wizmote.mac_address] == wizmote.sequence) {
-    return;
+    return true;
   }
   this->last_sequence_[wizmote.mac_address] = wizmote.sequence;
 
   this->on_button_->trigger(wizmote);
+  return true;
 }
 
-}  // namespace wizmote
-}  // namespace esphome
+}  // namespace esphome::wizmote
--- components/wizmote/wizmote.h
+++ components/wizmote/wizmote.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "esphome/components/esp_now/esp_now_component.h"
+#include "esphome/components/espnow/espnow_component.h"
 #include "esphome/core/automation.h"
+#include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
 
 #include <map>
 
-namespace esphome {
-namespace wizmote {
+namespace esphome::wizmote {
 
 typedef uint64_t MACAddress;
 
@@ -18,31 +18,27 @@ typedef struct __attribute__((__packed__)) WizMotePacket {
   };
 
   union {
-    uint8_t program;    // 0x91 for ON button, 0x81 for all others
+    uint8_t program;  // 0x91 for ON button, 0x81 for all others
     struct __attribute__((__packed__)) {
-      unsigned :4;
-      unsigned pairing:1;
-      unsigned :3;
+      unsigned : 4;
+      unsigned pairing : 1;
+      unsigned : 3;
     };
   };
-  uint32_t sequence;    // Incremental sequence number 32 bit unsigned integer LE
-  uint8_t dType1 = 32;  // Data type: button (32)
-  uint8_t button;       // Identifies which button is being pressed
-  uint8_t dType2 = 1;   // Data type: batteryLevel (1)
-  uint8_t batteryLevel; // WiZMote batteryLevel out of 100
-  uint8_t mac[4];       // CCM MAC (Message Authentication Code)
+  uint32_t sequence;     // Incremental sequence number 32 bit unsigned integer LE
+  uint8_t dType1 = 32;   // Data type: button (32)
+  uint8_t button;        // Identifies which button is being pressed
+  uint8_t dType2 = 1;    // Data type: batteryLevel (1)
+  uint8_t batteryLevel;  // WiZMote batteryLevel out of 100
+  uint8_t mac[4];        // CCM MAC (Message Authentication Code)
 
-  static inline WizMotePacket build(esp_now::ESPNowPacket espnow_packet) {
-    WizMotePacket packet;
-    memcpy(packet.bssid, espnow_packet.get_bssid().data(), 6);
-    memcpy(&packet.program, espnow_packet.get_data().data(), espnow_packet.get_data().size());
-    return packet;
-  }
+  static inline WizMotePacket build(const espnow::ESPNowRecvInfo &info, const uint8_t *data, uint8_t size);
 } WizMotePacket;
 
-class WizMoteListener : public esp_now::ESPNowListener {
+class WizMoteListener : public Component, public espnow::ESPNowBroadcastedHandler {
  public:
-  void on_esp_now_message(esp_now::ESPNowPacket packet) override;
+  void setup() override;
+  bool on_broadcasted(const espnow::ESPNowRecvInfo &info, const uint8_t *data, uint8_t size) override;
 
   Trigger<WizMotePacket> *get_on_button_trigger() { return this->on_button_; }
 
@@ -51,5 +47,4 @@ class WizMoteListener : public esp_now::ESPNowListener {
   Trigger<WizMotePacket> *on_button_ = new Trigger<WizMotePacket>();
 };
 
-}  // namespace wizmote
-}  // namespace esphome
+}  // namespace esphome::wizmote
