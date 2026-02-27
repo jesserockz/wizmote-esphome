8925f0efafa0bd0c4eba6955c9714a5cf1d48e3e
old-square-eyes <old.square.eyes@gmail.com>
2026-02-27 21:38:38 +1300

    Avoid memory bork with oversized external chatter packets

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -2,7 +2,14 @@
 
 namespace esphome {
 namespace wizmote {
+
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
+  // Drop oversized packets (like WiZ SpaceSense/status payloads) 
+  // to prevent heap memory corruption. Standard WiZmote packets are 13 bytes.
+  if (packet.get_data().size() > 20) {
+    return; 
+  }
+
   WizMotePacket wizmote = WizMotePacket::build(packet);
 
   if (this->last_sequence_[wizmote.mac_address] == wizmote.sequence) {
